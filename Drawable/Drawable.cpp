32504343b2754da2284d2b30eded4cabed861049
Ron <Ron@somedomain.com>
2012-07-29 12:58:32 -0700

    Added drawable class

---
+++ Drawable/Drawable.cpp
@@ -0,0 +1,82 @@
+#include "../Adafruit_WS2801/Adafruit_WS2801.h"
+#include "Drawable.h"
+
+/**********************************************************************************/
+
+// Constructor with colour
+Drawable::Drawable(Adafruit_WS2801 board, uint8_t yOff, uint8_t xOff, uint8_t w, uint8_t h) {
+	strip = board;
+	basePoint[0] = yOff;
+	basePoint[1] = xOff;
+	width = w;
+	height = h;
+	boundingBox = (uint32_t*) calloc(w * h, 4); // later I will figure out how to properly dynamically allocate/deallocate memory for multidimensional arrays
+}
+
+Drawable::~Drawable(void) {
+	free(boundingBox);
+}
+
+// Width of bounding box
+uint8_t Drawable::w(void) {
+	return width;
+}
+
+// Height of bounding box
+uint8_t Drawable::h(void) {
+	return height;
+}
+
+// Translate the drawable object to another position relative to current
+void Drawable::translate(int dy, int dx) {
+	basePoint[0] += dy;
+	basePoint[1] += dx;
+}
+// Set the upper left corner position of bounding box
+void Drawable::setPosition(uint8_t y, uint8_t x) {
+	basePoint[0] = y;
+	basePoint[1] = x;
+}
+
+// Set the colour of the drawing (affects only the non-transparent entries of bounding box array)
+void Drawable::setColour(uint32_t c) {
+	int i;
+	for (i = 0; i < w() * h(); i++) {
+		if (boundingBox[i] != 0) {
+			boundingBox[i] = c;
+		}
+	}
+}
+// Set the colour of a specific pixel in Drawable. Must be valid pixel, else will do nothing.
+void Drawable::spc(uint8_t i, uint8_t j, uint32_t c) {
+	if ( i < h() && i >= 0 && j < w() && j >= 0 ) {
+		boundingBox[i * w() + j] = c;
+	}
+}
+// Draw the drawable onto the board
+// If transparent == true, then entries with colour 0 will not be drawn to the screen (so the object will be transparent where it is not coloured), else it will overwrite.
+void Drawable::draw(bool transparent) {
+	int i,j;
+	
+	for (i = 0; i < h(); i++) {
+		for (j = 0; j < w(); j++) {
+			if (transparent && boundingBox[i * w() + j] > 0) 
+				strip.spc(i + basePoint[0],j + basePoint[1], boundingBox[i * w() + j]); // Remember, we are using matrix subscript notation, so yOff first, then xOff
+		}
+	}
+}
+
+// Returns the base point x-coordinate
+uint8_t Drawable::getBasePointX(void) {
+	return basePoint[1];
+}
+
+// REturns the base point y-coordinate
+uint8_t Drawable::getBasePointY(void) {
+	return basePoint[0];
+}
+
+// Returns pixel color in (i,j)th coordinate of bounding box array
+uint32_t Drawable::gpc(uint8_t i, uint8_t j) {
+	return boundingBox[i * w() + j];
+}
---
+++ Drawable/Drawable.h
@@ -0,0 +1,60 @@
+#ifndef __DRAWABLE_H_INCLUDED__
+#define __DRAWABLE_H_INCLUDED__
+
+#include "../Adafruit_WS2801/Adafruit_WS2801.h"
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
+// BIG NOTE: The grid is organized by matrix notation, so basepoint will be [y,x] not the reverse. Similarly, the functions related to the x,y position will be done using y then x.
+
+class Drawable {
+
+	public:
+
+		// Create drawable object, initialize upper left corner, bounding box and also pass the strip to which it will draw
+		Drawable(Adafruit_WS2801 board, uint8_t yOff, uint8_t xOff, uint8_t w, uint8_t h);
+		// Release memory (as needed):
+		~Drawable();
+	
+		void
+			// Translate the drawable object to another position relative to current
+			translate(int dy, int dx),
+			// Set the upper left corner position of bounding box
+			setPosition(uint8_t y, uint8_t x),
+			// Set the colour of the drawing (affects only the non-transparent entries of bounding box array)
+			setColour(uint32_t c),
+			// Set the colour of a specific pixel in Drawable
+			spc(uint8_t i, uint8_t j, uint32_t c),
+			// Draw the drawable onto the board
+			// If transparent == true, then entries with colour 0 will not be drawn to the screen (so the object will be transparent where it is not coloured), else it will overwrite.
+			draw(bool transparent = true);
+		uint8_t
+			// Width of bounding box
+			w(void),
+			// Height of bounding box
+			h(void),
+			// Returns the x-coordinate of base point
+			getBasePointX(void),
+			// Returns the y-coordinate of base point
+			getBasePointY(void);
+		uint32_t
+			// Returns pixel color in (i,j)th coordinate of bounding box array
+			gpc(uint8_t i, uint8_t j);
+			
+	private:
+			
+		uint8_t 
+			width,
+			height,
+			basePoint[2];
+		Adafruit_WS2801 
+			strip;
+		uint32_t  
+			*boundingBox;
+};
+
+#endif
